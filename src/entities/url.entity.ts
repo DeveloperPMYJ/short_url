46c4f9f25c0d8a6c77de61e7aa0ba7ad73feba7b
신유진 <julia011097@gmail.com>
2023-11-07 15:24:01 +0900

    url entity

---
+++ src/entities/url.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('dblog')
+export class DbLogEntity {
+    @PrimaryGeneratedColumn()
+id: number;
+
+
+    @Column({ type: 'varchar', length: 200, nullable: false })
+    originalUrl: string;
+
+    @Column({ type: 'varchar', length: 200, nullable: false })
+    shortenUrl: string;
+
+    @CreateDateColumn({ type: 'timestamp', nullable: false })
+    created_at: Date;
+
+    @Column({ type: 'varchar', length: 200, nullable: false })
+    user_ip: string;
+}
