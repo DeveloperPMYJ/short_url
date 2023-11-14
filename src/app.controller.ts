c706ec52f4442cf8b4a41aed9ac922d3691aa816
신유진 <julia011097@gmail.com>
2023-11-14 16:56:49 +0900

    app.controller 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UrlService } from './api/url.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private urlService: UrlService)  {}
+
+  // redirect api 
+  @Get(':hash')       // create 시 (original post 시 프로토콜까지 https, http인지 알려줘야), get api : http://localhost:3000/a1VNq74h
+  async redirectToOriginalUrl(
+      @Param('hash') hash: string, 
+      @Res() response) {
+  const originalUrl = await this.urlService.findOriginalUrlByShorten(hash);
+  response.redirect(302, originalUrl );
+}
+
 
   @Get()
   getHello(): string {
