2fc1becbfa3c729f6c17982fe6d2d8c82e2318ec
신유진 <julia011097@gmail.com>
2023-11-14 16:52:01 +0900

    Merge

--- src/api/url.controller.ts
+++ src/api/url.controller.ts
@@ -2,21 +2,11 @@ import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { UrlService } from './url.service';
 
 
-@Controller()  
+@Controller('shorten-url')  
 export class UrlController {
   constructor(private urlService: UrlService) {} 
-
-  // redirect api 
-  @Get(':hash')       // create 시 (original post 시 프로토콜까지 https, http인지 알려줘야), get api : http://localhost:3000/a1VNq74h
-  async redirectToOriginalUrl(
-      @Param('hash') hash: string, 
-      @Res() response) {
-  const originalUrl = await this.urlService.findOriginalUrlByShorten(hash);
-  response.redirect(302, originalUrl );
-}
-
   // short 생성 
-  @Post('shorten-url/create')
+  @Post('create')
   async shortenUrl(@Body() data: { originalUrl: string; userIp: string }) {
     const { originalUrl, userIp } = data;
 
