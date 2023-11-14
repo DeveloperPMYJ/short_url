d69bfd0415ba5a4053f67a8fc208f345536a77b2
신유진 <julia011097@gmail.com>
2023-11-14 16:01:09 +0900

    controller 수정

--- src/api/url.controller.ts
+++ src/api/url.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { UrlService } from './url.service';
 
-// "shorturl 생성 api"
+
 @Controller()  
 export class UrlController {
   constructor(private urlService: UrlService) {} 
 
+  // redirect api 
   @Get(':hash')       // create 시 (original post 시 프로토콜까지 https, http인지 알려줘야), get api : http://localhost:3000/a1VNq74h
   async redirectToOriginalUrl(
       @Param('hash') hash: string, 
@@ -14,7 +15,7 @@ export class UrlController {
   response.redirect(302, originalUrl );
 }
 
-  // short 생성 : original, short url, user IP 를 하나의 테이블에 저장
+  // short 생성 
   @Post('shorten-url/create')
   async shortenUrl(@Body() data: { originalUrl: string; userIp: string }) {
     const { originalUrl, userIp } = data;
@@ -28,5 +29,5 @@ export class UrlController {
     } catch (error) {
       return { error: error.message };
     }
-  } // shortenUrl 메서드에서,'UrlService'의 createShortUrl 메서드 호출 (shortUrl, await), return shorturl
+  } 
 }
--- src/api/url.service.ts
+++ src/api/url.service.ts
@@ -13,58 +13,51 @@ export class UrlService {
     private urlRepo: Repository<UrlEntity>, // Entity class 지정
     ) {}
 
-  //short url 생성
-async createShortUrl(originalUrl: string, userIp: string): Promise<string> {
-    // 1. user ip 로 하루 short url 변환 횟수 확인
-    const today = new Date();
-    today.setHours(0, 0, 0, 0);
+  //1. short url 생성
+    async createShortUrl(originalUrl: string, userIp: string): Promise<string> {
+        // user ip 로 하루 short url 변환 횟수 확인
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
 
-    //     // 변환횟수 (urlCount)
-    // const urlCount = await this.urlRepo // a) await = this.urlRepo 객체의 createQueryBuilder 메서드가 반환한 Promise를 대기하고 있음
-    //     .createQueryBuilder('url')
-    //     .where('url.userIp = :userIp', { userIp }) // userIp로 하루 short URL 변환 횟수 확인
-    //     .andWhere('url.createdAt >= :today', { today })
-    //     .getCount();                        // b) 그 후에 getCount() 메서드를 호출하여 해당 카운트를 얻는다
+        // 변환횟수 (urlCount)
+        const urlCount = await this.urlRepo 
+            .createQueryBuilder('url')
+            .where('url.userIp = :userIp', { userIp }) 
+            .andWhere('url.createdAt >= :today', { today })
+            .getCount();                    
 
-    //     // 변환 횟수 (urlCount) 30회 이상 -> throw NotFoundException (요청 제한)
-    // if (urlCount >= 30) {
-    //     throw new NotFoundException('Short URL 변환 횟수가 제한을 초과하였습니다');
-    // }
+        // 변환 횟수 (urlCount) 30회 이상 -> throw NotFoundException (요청 제한)
+        if (urlCount >= 30) {
+            throw new NotFoundException('Short URL 변환 횟수가 제한을 초과하였습니다');
+        }
 
-    // 2. short url
-    const baseUrl = 'http://localhost:3000/';
-    const hash = generateUniqueShortUrl();   //뒷부분 난수
-    const shortUrl = baseUrl + hash;
+        // short url
+        const baseUrl = 'http://localhost:3000/';
+        const hash = generateUniqueShortUrl();   //뒷부분 난수
+        const shortUrl = baseUrl + hash;
 
-    // console.log(shortUrl);
+        //데이터베이스에 'hash ,originalUrl, userIp' 저장 (repo)
+        await this.urlRepo
+            .createQueryBuilder()
+            .insert()
+            .into(UrlEntity)
+            .values({
+                originalUrl: originalUrl,
+                hash: hash,
+                userIp: userIp,
+            })
+            .execute();
 
-    //데이터베이스에 'hash ,originalUrl, userIp' 저장 (repo)
-    await this.urlRepo
-        .createQueryBuilder()
-        .insert()
-        .into(UrlEntity)
-        .values({
-            originalUrl: originalUrl,
-            hash: hash,
-            userIp: userIp,
-        })
-        .execute();
-
-    return shortUrl;
+        return hash;     // Promise 가 있기에 return 해줘야 함
     }
 
   // 2. "새로운 URL 리디렉션 api" server - redirect -> [service에서 originalUrl 호출]
 
   //  "originalUrl 호출" shortUrl에 해당하는 UrlEntity를 데이터베이스에서 찾기
-    async findOriginalUrlByShorten(
-        hash: string,
-    ): Promise<string | undefined> {
-    console.log(hash);
-        const urlEntity = await this.urlRepo.findOne({
-        where: { hash },
-        }); 
+    async findOriginalUrlByShorten(hash: string): Promise<string | undefined> {    
+        const urlEntity = await this.urlRepo.findOne({ where: { hash } }); 
     // console.log(urlEntity);
-    return urlEntity ? urlEntity.originalUrl : undefined;
+        return urlEntity ? urlEntity.originalUrl : undefined;
 }
 
     async redirectUrl(
